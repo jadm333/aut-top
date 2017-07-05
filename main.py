684bac3762fa6df77dd5cba1e520ad6e1f8a9d89
jadm333 <jadm333@gmail.com>
2017-07-05 02:14:11 -0500

    Update

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ for file_ in allFiles:
     del df['modification_date']
     del df['RT_temp']
     del df['is_retweeted']
-    df = df.loc[df['created_at_datetime'] > "2017-05-28"]
+    df = df.loc[df['created_at_datetime'] > "2017-05-27"]
     df['text'] = df['text'].apply(p.clean)
     df['text'].replace('', np.nan, inplace=True)
     df.dropna(subset=['text'], inplace=True)
@@ -74,10 +74,66 @@ del list_
 del path
 
 #%%
+import spacy
+nlp = spacy.load('es')
+#%%
+docs = list(df['text'])
+#%%
+%%time
+processed_docs = []    
+for doc in nlp.pipe(docs, n_threads=4, batch_size=100):
+    # Process document using Spacy NLP pipeline.
+    
+    ents = doc.ents  # Named entities.
+
+    # Keep only words (no numbers, no punctuation).
+    # Lemmatize tokens, remove punctuation and remove stopwords.
+    doc = [token.lemma_ for token in doc if token.is_alpha and not token.is_stop]
+
+    # Remove common words from a stopword list.
+    #doc = [token for token in doc if token not in STOPWORDS]
+
+    # Add named entities, but only if they are a compound of more than word.
+    doc.extend([str(entity) for entity in ents if len(entity) > 1])
+    
+    processed_docs.append(doc)
+
+#%%
+docs = processed_docs
+del processed_docs
+
+#%%
+# Compute bigrams.
+from gensim.models import Phrases
+# Add bigrams and trigrams to docs (only ones that appear 20 times or more).
+bigram = Phrases(docs, min_count=20)
+for idx in range(len(docs)):
+    for token in bigram[docs[idx]]:
+        if '_' in token:
+            # Token is a bigram, add to document.
+            docs[idx].append(token)
 
 
+#%%
+
+# Create a dictionary representation of the documents, and filter out frequent and rare words.
+
+from gensim.corpora import Dictionary
+dictionary = Dictionary(docs)
+
+# Remove rare and common tokens.
+# Filter out words that occur too frequently or too rarely.
+max_freq = 0.5
+min_wordcount = 20
+dictionary.filter_extremes(no_below=min_wordcount, no_above=max_freq)
+
+_ = dictionary[0]  # This sort of "initializes" dictionary.id2token.
 
 
+#%%
+# Vectorize data.
 
+# Bag-of-words representation of the documents.
+corpus = [dictionary.doc2bow(doc) for doc in docs]
 
-
+#%%
