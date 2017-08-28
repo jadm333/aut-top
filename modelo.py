18783db82b90052c61720b197a00b93cf939de50
jadm333 <jadm333@gmail.com>
2017-08-28 13:57:44 -0500

    Add script standalone

---
+++ modelo.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+@author: PEPE
+
+"""
+#%%
+
+#%% Paqueterias
+
+import os
+import pandas as pd
+import glob
+import os
+import preprocessor as p
+import numpy as np
+
+def function():
+    pass
