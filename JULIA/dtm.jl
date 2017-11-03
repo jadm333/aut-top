21844470f1391000a36aa5660062c30891823d0d
jadm333 <jadm333@gmail.com>
2017-11-03 14:53:44 -0600

    Dtm

--- JULIA/dtm.jl
+++ JULIA/dtm.jl
@@ -1,2 +1,20 @@
 ###Cargar datos
 using TopicModelsVB
+import Distributions.sample
+#Arreglar direccion
+corp = readcorp(:mac)
+
+corp.docs = vcat([sample(filter(doc -> round(doc.stamp / 100) == y, corp.docs), 400, replace=false) for y in 1984:2005]...)
+
+fixcorp!(corp, abr=100, len=10) # Remove words which appear < 100 times and documents of length < 10.
+
+basemodel = LDA(corp, 9)
+train!(basemodel, iter=150, chkelbo=151)
+
+# training...
+
+model = DTM(corp, 9, 200, basemodel)
+train!(model, iter=10) # This will likely take about an hour on a personal computer.
+                       # Convergence for all other models is worst-case quadratic,
+                       # while DTM convergence is linear or at best super-linear.
+# training...
